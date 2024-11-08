2d0f00278acc2c3db684a19f1372c42a106d202a
pablo-gomez-perez <pablojesus34@hotmail.com>
2024-11-07 20:04:24 -0600

    Cambios

---
+++ SystemVerilog/UART_selector/impl/pnr/cmd.do
@@ -0,0 +1,12 @@
+-d C:\Users\PABLO-LAP-LENOVO\Documents\gti programing projects\hdl\UartSelector\SystemVerilog\UART_selector\impl\gwsynthesis\UART_selector.vg
+-p GW1NR-9C-QFN88P-6
+-pn GW1NR-LV9QN88PC6/I5
+-cst C:\Users\PABLO-LAP-LENOVO\Documents\gti programing projects\hdl\UartSelector\SystemVerilog\UART_selector\src\UART_selector.cst
+-cfg C:\Users\PABLO-LAP-LENOVO\Documents\gti programing projects\hdl\UartSelector\SystemVerilog\UART_selector\impl\pnr\device.cfg
+-bit
+-tr
+-ph
+-timing
+-cst_error
+-correct_hold 1
+-route_maxfan 23
--- SystemVerilog/UART_selector/src/main.sv
+++ SystemVerilog/UART_selector/src/main.sv
@@ -1,14 +1,45 @@
 module main(input logic clk,
-            input logic reset,
-            input logic select,
-            input logic Rx_ESP_Master,
-            input logic Rx_Esp_Sim,
-            input logic Rx_Pc_17,
+            reset,
+            select,
+            Rx_ESP_Master,
+            Rx_Esp_Sim,
+            Rx_Pc_17,
             output logic Tx_Pc_18,
-            output logic Tx_Esp_Master,
-            output logic Tx_Esp_Sim,
-            output logic Led_Selector);
+            Tx_Esp_Master,
+            Tx_Esp_Sim,
+            Led_Selector,
+            Led_Reset);
 
-    
+    assign Led_Selector = select;
+    assign Led_Reset = reset;
+
+    uart_selector us(select, Rx_Pc_17, Rx_ESP_Master, Rx_Esp_Sim, Tx_Pc_18, Tx_Esp_Master, Tx_Esp_Sim);
+
+endmodule
+
+module triState(input logic a, select, output tri y);
+
+    assign y = select ? a : 1'bz;
+
+endmodule
+
+module mux_triState(input logic Rx_ESP_Master,Rx_Esp_Sim, select, output tri Tx_Pc_18);
+
+    triState tri_1(Rx_ESP_Master,select,Tx_Pc_18);
+    triState tri_2(Rx_Esp_Sim,~select,Tx_Pc_18);
+
+endmodule
+
+module demux(input logic Rx_PC_17, select, output logic Rx_ESP_Master, Rx_Esp_Sim);
+
+    assign Rx_ESP_Master = select ? Rx_PC_17 : 1;
+    assign Rx_Esp_Sim = select ? Rx_PC_17 : 1;
+
+endmodule
+
+module uart_selector(input logic select, Rx_PC_17, Rx_ESP_Master, Rx_Esp_Sim, output logic Tx_Pc_18, Tx_Esp_Master, Tx_Esp_Sim);
+
+    mux_triState mux(Rx_Esp_Sim, Rx_ESP_Master, select, Tx_Pc_18);
+    demux dmx(Tx_Pc_18,select,Tx_Esp_Sim, Tx_Esp_Master);
 
 endmodule
