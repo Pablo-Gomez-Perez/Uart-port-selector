878578463f36ec1cb4d7bdd0a0659d1ec1fc8f0a
pablo-gomez-perez <pablojesus34@hotmail.com>
2024-11-07 18:49:35 -0600

    First commit

---
+++ SystemVerilog/UART_selector/src/main.sv
@@ -0,0 +1,14 @@
+module main(input logic clk,
+            input logic reset,
+            input logic select,
+            input logic Rx_ESP_Master,
+            input logic Rx_Esp_Sim,
+            input logic Rx_Pc_17,
+            output logic Tx_Pc_18,
+            output logic Tx_Esp_Master,
+            output logic Tx_Esp_Sim,
+            output logic Led_Selector);
+
+    
+
+endmodule
